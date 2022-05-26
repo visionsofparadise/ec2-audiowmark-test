cfdf5086eba8cb1dc91218ea669fb42f768a080a
Matt <visionsofparadise@pm.me>
2022-05-26 22:58:24 +0100

    test

---
+++ lib/ec2-audiowmark-test-stack.ts
@@ -0,0 +1,76 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as cwLogs from "aws-cdk-lib/aws-logs";
+
+export class Ec2AudiowmarkTestStack extends Stack {
+  get availabilityZones(): string[] {
+    return [
+      "us-east-1a",
+      "us-east-1b",
+      "us-east-1c",
+      "us-east-1d",
+      "us-east-1e",
+      "us-east-1f",
+    ];
+  }
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "VPC", {
+      natGateways: 0,
+      cidr: "10.0.0.0/16",
+      maxAzs: 2,
+    });
+
+    const logGroup = new cwLogs.LogGroup(this, `LogGroup`, {
+      retention: cwLogs.RetentionDays.ONE_WEEK,
+    });
+
+    const instance = new ec2.Instance(this, `EC2Instance`, {
+      instanceName: "audiowmark-test",
+      vpc,
+      vpcSubnets: {
+        subnets: vpc.publicSubnets,
+      },
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.NANO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+      allowAllOutbound: true,
+      blockDevices: [
+        {
+          deviceName: "/dev/sda1",
+          volume: ec2.BlockDeviceVolume.ebs(10),
+        },
+      ],
+      userData: ec2.UserData.forLinux({
+        shebang: `#!/bin/bash
+
+sudo yum update -y
+sudo yum install nodejs npm -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+
+git clone https://github.com/swesterfeld/audiowmark.git
+cd audiowmark
+docker build -t audiowmark .
+cd ..
+
+git clone https://github.com/visionsofparadise/ec2-audiowmark-test.git
+cd ec2-audiowmark-test
+npm i
+npm run compile
+
+LOG_GROUP_NAME=${logGroup.logGroupName} node service/index.js
+`,
+      }),
+      userDataCausesReplacement: true,
+    });
+
+    instance.connections.allowFromAnyIpv4(ec2.Port.tcp(4000));
+    logGroup.grantWrite(instance.grantPrincipal);
+  }
+}
---
+++ service/index.ts
@@ -0,0 +1,71 @@
+import * as AWS from "aws-sdk";
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const path = require("path");
+import * as express from "express";
+import * as cors from "cors";
+import * as bodyParser from "body-parser";
+
+const cloudwatchLogs = new AWS.CloudWatchLogs({
+  apiVersion: "2014-03-28",
+  region: "us-east-1",
+});
+
+const api = express();
+
+api.use(cors());
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(bodyParser.json());
+
+api.get("/", async (_, res) => res.sendStatus(200));
+
+const port = 4000;
+
+api.listen(port, async () => console.log(`Server listening on port ${port}`));
+
+const main = async () => {
+  const logGroupName = process.env.LOG_GROUP_NAME! || "test";
+  const logStreamName = `leaklock/ec2-audiowmark-test/${new Date().getTime()}`;
+
+  await cloudwatchLogs
+    .createLogStream({
+      logGroupName,
+      logStreamName,
+    })
+    .promise();
+
+  const log = async (message: string) =>
+    cloudwatchLogs
+      .putLogEvents({
+        logGroupName,
+        logStreamName,
+        logEvents: [
+          {
+            timestamp: new Date().getTime(),
+            message,
+          },
+        ],
+      })
+      .promise();
+
+  try {
+    const {
+      stderr,
+    } = await exec(
+      "docker run -v ~/ec2-audiowmark-test/data:/data --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677",
+      { cwd: "~/audiowmark" }
+    );
+
+    if (path.existsSync("data/test-out.wav")) {
+      await log("success");
+    } else {
+      throw stderr;
+    }
+  } catch (error) {
+    await log(error);
+  }
+
+  return;
+};
+
+main();
