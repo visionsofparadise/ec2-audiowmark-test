0bc2c16fef09ee827dba0fb51d98c82175ac6a34
visionsofparadise@pm.me <visionsofparadise@pm.me>
2022-05-27 16:28:08 +0100

    diag

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,6 +2,7 @@ import * as AWS from "aws-sdk";
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 const path = require("path");
+const fs = require("fs");
 
 const cloudwatchLogs = new AWS.CloudWatchLogs({
   apiVersion: "2014-03-28",
@@ -23,33 +24,35 @@ const main = async () => {
 
 	logs.push({ message: 'main started', timestamp: new Date().getTime() })
 
-	logs.push({ message: `dirname ${__dirname}`, timestamp: new Date().getTime() })
+	logs.push({ message: JSON.stringify(process.env, null, 4), timestamp: new Date().getTime() })
 
-	const dataPath = path.resolve(__dirname, 'dist/data')
+	logs.push({ message: `path cwd ${process.cwd()}`, timestamp: new Date().getTime() })
+
+	const dataPath = path.resolve(process.cwd(), 'dist/data')
 
 	logs.push({ message: `data ${dataPath}`, timestamp: new Date().getTime() })
 
-	const cwdPath = path.resolve(__dirname, '../audiowmark')
-
-	logs.push({ message: `cwd ${cwdPath}`, timestamp: new Date().getTime() })
-
-	const outPath = path.resolve(dataPath, "test-out.wav")
-
-	logs.push({ message: `out ${cwdPath}`, timestamp: new Date().getTime() })
-
   try {
     const {
-      stderr,
+      stdout, stderr,
     } = await exec(
       `docker run -v ${dataPath}:/data --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677`,
-      { cwd: cwdPath }
-    );
+      { cwd: '../audiowmark' }
+		);
 
-    if (path.existsSync(outPath)) {
-			logs.push({ message: 'success', timestamp: new Date().getTime() })
-    } else {
-      throw stderr;
-    }
+		logs.push({ message: `stdout ${JSON.stringify(stdout, null, 4)}`, timestamp: new Date().getTime() })
+		logs.push({ message: `stderr ${JSON.stringify(stderr, null, 4)}`, timestamp: new Date().getTime() })
+		
+		if (Object.keys(stderr).keys.length === 0) {
+			fs.access('../data/test-out.wav', fs.constants.R_OK, (error: any) => {
+				if (error) throw error
+
+				logs.push({ message: 'success', timestamp: new Date().getTime() })
+			});
+
+		} else {
+			throw stderr
+		}
   } catch (error) {
     logs.push({ message: JSON.stringify(error, null, 4), timestamp: new Date().getTime() })
 	}
