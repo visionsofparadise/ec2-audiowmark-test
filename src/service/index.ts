ec1095cb1726ba01adc0e6bc6cad071d3a76aacb
visionsofparadise@pm.me <visionsofparadise@pm.me>
2022-05-27 23:48:25 +0100

    txt

--- src/service/index.ts
+++ src/service/index.ts
@@ -27,7 +27,7 @@ const main = async () => {
 
 	try {
     const output = await exec(
-      'docker run -v /home/ubuntu/ec2-audiowmark-test/data:/data --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677'
+      "docker run -v /home/ubuntu/ec2-audiowmark-test/data:/data --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677"
 		);
 
 		logs.push({ message: `output ${JSON.stringify(output, null, 4)}`, timestamp: new Date().getTime() })
