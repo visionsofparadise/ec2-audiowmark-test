cf0e6ab7d9e0703f794e0bca8b2b4c94c56ca88d
visionsofparadise@pm.me <visionsofparadise@pm.me>
2022-05-28 02:51:23 +0100

    mount

--- src/service/index.ts
+++ src/service/index.ts
@@ -32,7 +32,7 @@ const main = async () => {
 			return
 		});
 
-    const output = await exec('docker run -v /home/ubuntu/ec2-audiowmark-test/data:/data --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677');
+    const output = await exec(`docker run --mount 'type=bind,src=/home/ubuntu/ec2-audiowmark-test/data,dst=/data' --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677`);
 
 		logs.push({ message: `output ${JSON.stringify(output, null, 4)}`, timestamp: new Date().getTime() })
 		
