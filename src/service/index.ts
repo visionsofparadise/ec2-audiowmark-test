a50de35740b1214c0860884a4cf3220ea18884e9
visionsofparadise@pm.me <visionsofparadise@pm.me>
2022-05-28 01:30:21 +0100

    filecheck

--- src/service/index.ts
+++ src/service/index.ts
@@ -26,14 +26,6 @@ const main = async () => {
 	logs.push({ message: JSON.stringify(process.env, null, 4), timestamp: new Date().getTime() })
 
 	try {
-		// fs.access('data/test.wav', fs.constants.R_OK, (error: any) => {
-		// 	if (error) throw error
-
-		// 	logs.push({ message: 'test.wav is readable', timestamp: new Date().getTime() })
-
-		// 	return
-		// });
-
     const output = await exec('docker run -v /home/ubuntu/ec2-audiowmark-test/data:/data --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677');
 
 		logs.push({ message: `output ${JSON.stringify(output, null, 4)}`, timestamp: new Date().getTime() })
--- src/service/index2.ts
+++ src/service/index2.ts
@@ -4,6 +4,14 @@ const fs = require("fs");
 
 const main = async () => {
 	try {
+		fs.access('data/test.wav', fs.constants.R_OK, (error: any) => {
+			if (error) throw error
+
+			console.log('test.wav is readable')
+
+			return
+		});
+
     const output = await exec('docker run -v /home/ubuntu/ec2-audiowmark-test/data:/data --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677');
 
 		console.log(JSON.stringify(output, null, 4))
