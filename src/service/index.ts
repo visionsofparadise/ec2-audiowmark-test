f17ec721365d76c441d404b69c6357f077169526
visionsofparadise@pm.me <visionsofparadise@pm.me>
2022-05-28 02:11:59 +0100

    fc

--- src/service/index.ts
+++ src/service/index.ts
@@ -27,11 +27,11 @@ const main = async () => {
 		fs.access('data/test.wav', fs.constants.R_OK, (error: any) => {
 			if (error) throw error
 
-			console.log('test.wav is readable')
+			logs.push({ message: 'test.wav is readable', timestamp: new Date().getTime() })
 
 			return
 		});
-		
+
     const output = await exec('docker run -v /home/ubuntu/ec2-audiowmark-test/data:/data --rm -i audiowmark add test.wav test-out.wav 0123456789abcdef0011223344556677');
 
 		logs.push({ message: `output ${JSON.stringify(output, null, 4)}`, timestamp: new Date().getTime() })
